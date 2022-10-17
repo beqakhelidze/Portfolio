198c308455b23035c6902fae5833e335012c6d99
beqakhelidze <khelidzeb@gmail.com>
2022-10-18 00:43:56 +0400

    Responsive Design is added!

--- index.js
+++ index.js
@@ -4,19 +4,18 @@
 const activeChanger = (element) => {
     document.querySelector(".active").classList.remove("active");
     element.classList.add("active");
+    document.getElementById("navbarNav").classList.toggle("show");
 }
 
 const copyNumber = () => {
     var copyText = "+995 579 44 12 44";
     navigator.clipboard.writeText(copyText);
-    alert("Copied the text: " + copyText);
   }
 
 const loadScript = () => {    
     document.querySelectorAll(".nav-item").forEach(element => {
         element.addEventListener("click", () => activeChanger(element))
     });
-
     document.getElementById("phone_number").addEventListener("click", copyNumber)
 }
 
